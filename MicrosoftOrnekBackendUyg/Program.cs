0a7098d09892c98895d7f06051ab4f5629c6c34a
Muhammed KARADAŞ <31390903+MuhammedKaradas@users.noreply.github.com>
2026-02-05 23:22:36 +0300

    chore: remove comments

--- MicrosoftOrnekBackendUyg/Program.cs
+++ MicrosoftOrnekBackendUyg/Program.cs
@@ -35,10 +35,7 @@ namespace MicrosoftOrnekBackendUyg
                    new SqlColumn("UserName", SqlDbType.VarChar)
                }
             };
-
-            //Data Source=tcp:invoicecollection.database.windows.net,1433;Initial Catalog=MicrosoftOrnekBackendUyg_db;User Id=dbadmin@invoicecollection;Password=123456Seven_
-            //Data Source=DESKTOP-GA0KMBM;Initial Catalog=InvoiceCollectionNew;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False
-            //
+            
             Log.Logger = new LoggerConfiguration()
              .WriteTo.Console()
              .WriteTo.Debug(outputTemplate: DateTime.Now.ToString())
@@ -51,33 +48,14 @@ namespace MicrosoftOrnekBackendUyg
                 sinkOptions: new MSSqlServerSinkOptions { TableName = "Log" },
                 null, null, LogEventLevel.Information, null, null, null, null)
              .MinimumLevel.Override("Microsoft", LogEventLevel.Error)
-             //.MinimumLevel.Information()
              .CreateLogger();
 
             Serilog.Debugging.SelfLog.Enable(msg =>
             { 
                 Debug.Print(msg);
-                //Debugger.Break();
             });
 
-
             CreateHostBuilder(args).Build().Run();
-
-            //try
-            //{
-            //    CreateHostBuilder(args).Build().Run();
-            //    return;
-            //}
-            //catch (Exception ex)
-            //{
-            //    Log.Fatal(ex, "Host terminated unexpectedly");
-            //    return;
-            //}
-            //finally
-            //{
-            //    Log.CloseAndFlush();
-            //}
-
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
